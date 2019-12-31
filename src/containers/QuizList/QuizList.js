f9f2987163d1c965d9a898b8d90c3ee35a82a88d
Ivan Nasanovich <37714995+Uolary@users.noreply.github.com>
2019-12-31 12:13:18 +0300

    added router

---
+++ src/containers/QuizList/QuizList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class QuizList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Quiz list</h1>
+      </div>
+    )
+  }
+}
+
+export default QuizList;
